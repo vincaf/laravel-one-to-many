0c34cdcc0c444304938310649c082579324c984d
vincaf <vincenzocafassog@gmail.com>
2022-09-22 00:38:47 +0200

    feat(UsersTable): create users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $myUser = new User();
+        $myUser->name = 'vincenzo';
+        $myUser->email = 'vincenzo@boolean.it';
+        $myUser->password = Hash::make('Password');
+        $myUser->save();
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $user->name = $faker->userName();
+            $user->email = $faker->unique()->email();
+            $user->password = Hash::make($faker->password());
+            $user->save();
+        }
+    }
+}
