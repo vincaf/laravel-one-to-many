f25098a21c59fdc8cafdb812f4ffc9b2916e80d5
vincaf <vincenzocafassog@gmail.com>
2022-09-22 02:44:26 +0200

    feat(User): add relationship w/ userDetail & posts

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function userDetail(){
+        return $this->hasOne('App\Models\UserDetail');
+    }
+
+    public function posts(){
+        return $this->hasMany('App\Models\Post');
+    }
 }
